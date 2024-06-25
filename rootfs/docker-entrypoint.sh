d67cd58965377e335f71d4749ac96253d8bf3961
Socheat Sok <socheatsok78@gmail.com>
2024-06-25 12:05:15 +0700

    Advance loki config

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -16,9 +16,10 @@ PROMTAIL_LOGFORMAT=${PROMTAIL_LOGFORMAT:-"logfmt"}
 # The config of clients of the Promtail server
 # -- Loki server configuration
 GF_LOKI_SCHEME=${GF_LOKI_SCHEME:-"http"}
+GF_LOKI_HOST=${GF_LOKI_PORT:-"loki"}
 GF_LOKI_PORT=${GF_LOKI_PORT:-"3100"}
-GF_LOKI_ADDR=${GF_LOKI_ADDR:-"loki:${GF_LOKI_PORT}"}
-GF_LOKI_URL=${GF_LOKI_URL:-"${GF_LOKI_SCHEME}://${GF_LOKI_ADDR}/loki/api/v1/push"}
+GF_LOKI_ADDR=${GF_LOKI_ADDR:-"${GF_LOKI_SCHEME}://${GF_LOKI_HOST}:${GF_LOKI_PORT}"}
+GF_LOKI_URL=${GF_LOKI_URL:-"${GF_LOKI_ADDR}/loki/api/v1/push"}
 PROMTAIL_CLIENT_URL=${PROMTAIL_CLIENT_URL:-$GF_LOKI_URL}
 
 # -- Configures where Promtail will save it's positions file, to resume reading after restarts.
